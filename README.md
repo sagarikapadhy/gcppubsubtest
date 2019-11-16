ddfe61dc755cfc87ab420c54e86fefbb7e6e334f
sagarika padhy <sagarika.padhy478@gmail.com>
2019-11-16 14:09:00 +1100

    added readme.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # gcppubsubtest
+To run this app successfully 
+1. Create your own topic and subscription in google cloud console
+2. Create custom role and assign required permission.
+3. Create own service account and assign the custom role to it
+4. Use google cloud sdk to authenticate your user
+5. Import service account private key into your project
+6. Make sure topic has required permision, i.e your user should be added to the custom role if you have created
+
---
+++ src/main/java/test/TestController.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+public class TestController {
+    // tag::autowireGateway[]
+    @Autowired
+    private PubSubApplication.PubsubOutboundGateway messagingGateway;
+    // end::autowireGateway[]
+
+    @PostMapping("/publishMessage")
+    public RedirectView publishMessage(@RequestParam("message") String message) {
+        messagingGateway.sendToPubsub(message);
+        return new RedirectView("/");
+    }
+}
